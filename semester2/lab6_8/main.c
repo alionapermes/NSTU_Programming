7ea6dd4ad267b6c5fba6354a64504ab722882881
Linadil <cpp.is.power@gmail.com>
2021-02-22 20:35:46 +0700

    lab6_var8

--- semester2/lab6_8/main.c
+++ semester2/lab6_8/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*
 int main(int argc, char** argv) {
     /// reading arguments
     int counter = 0;
@@ -82,6 +83,69 @@ int main(int argc, char** argv) {
     free(str);
     free(file_inp);
     free(file_out);
+    free(arg_file);
+    free(arg_length);
+
+    return 0;
+}
+*/
+
+
+int main(int argc, char** argv) {
+    /// reading arguments
+    int temp = 0;
+    while (argv[1][temp++] != '\0') {}
+    char* arg_file = malloc(temp * sizeof(char));
+
+    char target = argv[2][0];
+
+    temp = 0;
+    do {
+        arg_file[temp] = argv[1][temp];
+    } while (argv[1][temp++] != '\0');
+    /// arguments read
+
+    char end[5] = ".out\0";
+    int  length = 0; // index of the last file's symbol
+
+    // counting arg_'s length
+    while (arg_file[length] != '\0') {
+        length++;
+    }
+
+    char* file_inp = malloc(sizeof(char) * length); // input file
+    char* file_out = malloc(length + sizeof(end) / sizeof(char)); // output file
+
+    /// prepearing file names
+    int ltr;
+    for (ltr = 0; ltr != length; ltr++) {
+        file_inp[ltr] = arg_file[ltr];
+    }
+
+    for (int i = 0; file_inp[i] != '\0'; i++) {
+        file_out[i] = file_inp[i];
+    }
+    for (int i = 0; end[i] != '\0'; i++) {
+        file_out[ltr] = end[i];
+        ltr++;
+    }
+    /// prepearing was finished
+
+    FILE* file = fopen(file_inp, "r");
+    FILE* out  = fopen(file_out, "w");
+
+    // file processing
+    while (!feof(file)) { // check if end of file
+        char current = fgetc(file);
+        fprintf(out, "%c", (current == target ? ' ' : current)); // writing string into output file
+    }
+
+    fclose(file);
+    fclose(out);
+
+    free(file_inp);
+    free(file_out);
+    free(arg_file);
 
     return 0;
 }
