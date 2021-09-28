446ab94274c3fe687930526892b64aff24771464
Linadil <cpp.is.power@gmail.com>
2021-09-28 23:47:40 +0700

    lab7_13 done

--- semester3/mytools.h
+++ semester3/mytools.h
@@ -10,22 +10,25 @@
 #define MSG_SIZE 128
 
 #define TRY(funcName) \
-{ \
-  if (funcName == -1) \
-    { \
-      puts(error_msg(errno)); \
-      exit(-1); \
-    } \
-}
+if (funcName == -1) \
+  { \
+    puts(error_msg(errno)); \
+    exit(-1); \
+  } \
+
+#define PTRY(funcName) \
+if (funcName != 0) \
+  { \
+    puts(error_msg(errno)); \
+    exit(-1); \
+  } \
 
 #define ERR_CHECK(value, errSign) \
-{ \
-  if (value == errSign) \
-    { \
-      puts(error_msg(errno)); \
-      exit(-1); \
-    } \
-}
+if (value == errSign) \
+  { \
+    puts(error_msg(errno)); \
+    exit(-1); \
+  } \
 
 char* error_msg(int error);
 char* status_msg(int status);
