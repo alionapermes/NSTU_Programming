19c55d6f32c59988d0923d59d2ad2f6b97d8a98f
Linadil <cpp.is.power@gmail.com>
2021-11-22 22:44:51 +0700

    add server tools

---
+++ semester3/coursework/src/server/tools.c
@@ -0,0 +1,13 @@
+#include "tools.h"
+
+#include <stdint.h>
+#include <arpa/inet.h>
+
+
+void
+init_sockaddr_in(struct sockaddr_in* addr, uint16_t port)
+{
+    addr->sin_family = AF_INET;
+    addr->sin_port = htons(port);
+    addr->sin_addr.s_addr = htonl(INADDR_ANY);
+}
---
+++ semester3/coursework/src/server/tools.h
@@ -0,0 +1,9 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+
+void
+init_sockaddr_in(struct sockaddr_in* addr, uint16_t port);
+
+
+#endif
