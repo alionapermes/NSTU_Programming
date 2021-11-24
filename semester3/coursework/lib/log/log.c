bd37eb304eb7a46f9eaac8c0bdfe6846f0a34f51
Linadil <cpp.is.power@gmail.com>
2021-11-24 20:04:23 +0700

    done

--- semester3/coursework/lib/log/log.c
+++ semester3/coursework/lib/log/log.c
@@ -1,10 +1,26 @@
 #include "log.h"
 
-#include "../scrsys/io.h"
-
 
 void
-write_log()
+write_log(const char* path, const char* content, size_t count)
 {
-    s_write();
+    int fd = s_open(path, O_CREAT | O_APPEND | O_WRONLY, 0644);
+    char* msg = calloc(sizeof(char), count + TIMESTAMP_LENGTH);
+
+    build_msg(msg, content);
+    s_write(fd, msg, strlen(msg));
+
+    free(msg);
+}
+
+char*
+build_msg(char* msg, const char* content)
+{
+    const time_t timer = time(NULL);
+    char* timestamp = ctime(&timer);
+
+    timestamp[strlen(timestamp) - 1] = '\0'; 
+    sprintf(msg, "[%s]: %s\n", timestamp, content);
+
+    return msg;
 }
--- semester3/coursework/lib/log/log.h
+++ semester3/coursework/lib/log/log.h
@@ -1,14 +1,21 @@
 #ifndef ERRORLOG_H
 #define ERRORLOG_H
 
-#define LOG_ERROR 1
-#defene LOG_ACTION 2
-#define LOG_DEBUG 3
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include "../scrsys/io.h"
+#include "../scrsys/error.h"
+
+#define TIMESTAMP_LENGTH 30
 
 
 void
-write_log(const char* path, int type);
+write_log(const char* path, const char* content, size_t count);
+
+char*
+build_msg(char* msg, const char* content);
 
 
 #endif
-
