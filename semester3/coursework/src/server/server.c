8c788ffc2e752f4afc3c7eb9971c4af7158ea27c
Linadil <cpp.is.power@gmail.com>
2021-11-24 22:54:58 +0700

    add work info printing

--- semester3/coursework/src/server/server.c
+++ semester3/coursework/src/server/server.c
@@ -9,6 +9,8 @@
 int
 main(int argc, char** argv)
 {
+    printf("[+] server started");
+
     struct sockaddr_in addr;
     size_t files_count = 0;
     int listenfd = 0;
@@ -17,17 +19,22 @@ main(int argc, char** argv)
 
 
     init_sockaddr_in(&addr, PORT);
-
     listenfd = s_socket(AF_INET, SOCK_STREAM, 0);
+    printf("[+] socket created\n");
+
 
     s_bind(listenfd, (struct sockaddr*)&addr, sizeof(addr));
     s_listen(listenfd, 1);
+    printf("[+] listening started\n");
 
     connfd = s_accept(listenfd, NULL, NULL);
+    printf("[+] connection accepted\n");
     
     //
 
     s_close(connfd);
+    printf("[+] socket closed\n");
 
+    printf("[+] server stopped\n");
     return 0;
 }
