e824295e251b07d5127289f676acfd5e3b12c024
Linadil <cpp.is.power@gmail.com>
2021-11-25 23:34:33 +0700

    add receive_data func

--- semester3/coursework/src/server/tools.c
+++ semester3/coursework/src/server/tools.c
@@ -8,3 +8,26 @@ init_sockaddr_in(struct sockaddr_in* addr, uint16_t port)
     addr->sin_port = htons(port);
     addr->sin_addr.s_addr = htonl(INADDR_ANY);
 }
+
+size_t
+receive_data(int sockfd, char*** filenames)
+{
+    size_t files_count = 0;
+
+    s_read(sockfd, &files_count, sizeof(files_count));
+
+    for (size_t n = 0; n < files_count; n++) {
+        size_t len = 0;
+        ssize_t bytes = 0;
+
+        (*filenames)[n] = calloc(sizeof(char), MAX_NAME);
+
+        printf("reading from client...\n");
+        s_read(sockfd, &len, sizeof(len));
+
+        bytes = s_read(sockfd, (*filenames)[n], len);
+        printf("[+] received: %s\n", (*filenames)[n]);
+    }
+
+    return files_count;
+}
--- semester3/coursework/src/server/tools.h
+++ semester3/coursework/src/server/tools.h
@@ -4,9 +4,14 @@
 #include <stdint.h>
 #include <arpa/inet.h>
 
+#define MAX_NAME 255
+
 
 void
 init_sockaddr_in(struct sockaddr_in* addr, uint16_t port);
+
+size_t
+receive_data(int connfd, char*** filenames);
 
 
 #endif
