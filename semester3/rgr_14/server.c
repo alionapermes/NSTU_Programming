4dca23290ae4b9cea1ed2640f3f296919a8b01bd
alionapermes <alionapermes@gmail.com>
2021-12-26 06:10:12 +0700

    serv fix

--- semester3/rgr_14/server.c
+++ semester3/rgr_14/server.c
@@ -2,7 +2,27 @@
 
 #include "headers.h"
 
-int main(int argc, char** argv)
+void startServ();
+
+int main()
+{
+    int pid = 0;
+    try((pid = fork()), -1);
+
+    if (pid == 0) {
+        umask(0);
+        try(setsid(), -1);
+        try(close(STDIN_FILENO), -1);
+        try(close(STDOUT_FILENO), -1);
+        try(close(STDERR_FILENO), -1);
+
+        startServ();
+    }
+    
+    return 0;
+}
+
+void startServ()
 {
     writeLog(INFO, "=== SERVER STARTED ===");
     sockaddr_in6 svaddr, claddr;
@@ -25,7 +45,7 @@ int main(int argc, char** argv)
         pid_t pid;
         int status, resp;
         char c[2] = {0};
-        char s[80] = {0};
+        char s[80+BUF_SIZE] = {0};
 
         try(recvfrom(sfd, c, 2, 0, (sockaddr*)&claddr, &len), -1);
         snprintf(s, 79, "Replacesment char received [%c]", *c);
@@ -34,8 +54,8 @@ int main(int argc, char** argv)
         printf("char: %s\n", c);
 #endif
         try((bytes = recvfrom(sfd, buf, BUF_SIZE, 0, (sockaddr*)&claddr, &len)), -1);
-        memset(s, 0, 80);
-        snprintf(s, 79, "File name received [%s]", buf);
+        memset(s, 0, 80+BUF_SIZE);
+        snprintf(s, 79+BUF_SIZE, "File name received [%s]", buf);
         writeLog(INFO, s);
 #ifdef DEBUG
         printf("buf: %s\n", buf);
@@ -44,8 +64,8 @@ int main(int argc, char** argv)
         if (inet_ntop(AF_INET6, &claddr.sin6_addr, claddr_str, INET6_ADDRSTRLEN) == NULL) {
             writeLog(ERROR, "Couldn't convert client address to string");
         } else {
-            char m[80] = {0};
-            snprintf(m, 79, "Server received %ld bytes from (%s, %u)\n", bytes, claddr_str, ntohs(claddr.sin6_port));
+            char m[BUF_SIZE] = {0};
+            snprintf(m, BUF_SIZE-1, "Server received %ld bytes from (%s, %u)\n", bytes, claddr_str, ntohs(claddr.sin6_port));
             writeLog(INFO, m);
         }
 
@@ -63,6 +83,4 @@ int main(int argc, char** argv)
         }
 // #####
     }
-
-    return 0;
 }
