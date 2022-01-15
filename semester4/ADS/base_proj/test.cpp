c4794c5fac9b21d50de3d518acbe258a8d518469
Linadil <cpp.is.power@gmail.com>
2022-01-15 12:45:47 +0700

    add base_proj

---
+++ semester4/ADS/base_proj/test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+using namespace std;
+
+
+TEST(suite, name)
+{
+    //
+}
+
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
