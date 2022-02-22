8a5cb7589fdf618d722494dee08a3374f122ff50
alionapermes <alionapermes@gmail.com>
2022-02-22 09:33:58 +0700

    add modeling dir

---
+++ semester4/modeling/base_proj/main.cpp
@@ -0,0 +1,20 @@
+#include "container.hpp"
+
+#include "gtest/gtest.h"
+#include "tl/expected.hpp"
+
+using namespace std;
+
+
+TEST(suite, name)
+{
+    //
+}
+
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
