1e0e3394477519ef886a7c63e6d8d9ce278ca181
alionapermes <alionapermes@gmail.com>
2022-02-22 09:27:47 +0700

    ladno

---
+++ semester4/ADS/lab1/v3/container.hpp
@@ -0,0 +1,24 @@
+// Вариант 3 - односвязная структура данных
+// на базе массива с индексными указателями
+
+#pragma once
+
+#include <cstddef>
+#include <iterator>
+
+#include "tl/expected.hpp"
+
+
+template<typename T>
+class list_v3
+{
+public:
+    list_v3() = default;
+
+    ~list_v3() = default;
+
+private:
+    T** head     = nullptr;
+    T** pointers = nullptr;
+};
+
