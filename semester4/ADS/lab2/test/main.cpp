e41bc2efbf62d75d174afb8265ed5e13f5a040b8
alionapermes <alionapermes@gmail.com>
2022-03-29 01:34:13 +0700

    init

---
+++ semester4/ADS/lab2/test/main.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+#include "bst.hpp"
+
+
+TEST(group, case)
+{
+    //
+}
+
+
+int
+main(int argc, char** argv)
+{
+    testing::InitGooglTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
