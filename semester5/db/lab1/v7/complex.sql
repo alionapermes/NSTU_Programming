36c7716b4494e71b47af48cdcf5e069616a9dfe6
alionapermes <alionapermes@gmail.com>
2022-09-21 19:10:16 +0700

    db lab_1v7

---
+++ semester5/db/lab1/v7/complex.sql
@@ -0,0 +1,23 @@
+CREATE TABLE lab_1v7.complex AS (
+    SELECT
+        "order".id                        AS order_id,
+        "order".date                      AS date,
+        "order".distance                  AS distance,
+        "car".km_price                    AS fare,
+        "car".km_price * "order".distance AS total_price,
+        "driver".surname                  AS driver_surname,
+        "city".name                       AS driver_city,
+        "car".model                       AS car_model
+    FROM
+        lab_1v7.order AS "order"
+    LEFT JOIN
+        lab_1v7.driver AS "driver"
+        ON "order".driver_id = driver.id
+    LEFT JOIN
+        lab_1v7.city AS "city"
+        ON "driver".city_id = city.id
+    LEFT JOIN
+        lab_1v7.car AS "car"
+        ON "driver".car_id = car.id
+);
+
