51f4f6e9a4e8de38b99eb2ac855582cc606c6967
alionapermes <alionapermes@gmail.com>
2022-12-01 02:25:23 +0700

    lab5 started

---
+++ semester5/db/v3/src/lab5/common.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION add_n(n INTEGER) RETURNS CHAR AS $$
+BEGIN
+    FOR i IN 0..n LOOP
+        INSERT INTO
+            car(model, km_price, number)
+        VALUES
+            (
+                (SELECT (ARRAY ['Toyota','Ford','Nissan'])[FLOOR(RANDOM() * 3 + 1)]),
+                (SELECT FLOOR(RANDOM() * 200 + 100)),
+                (SELECT CONCAT('a', FLOOR(RANDOM() * 898 + 101), 'aa'))
+            );
+    END LOOP;
+
+    RETURN n || ' items inserted';
+END;
+$$ LANGUAGE "plpgsql";
+
+SELECT add_n(1000);
+
+EXPLAIN ANALYZE
+    SELECT * FROM "car" WHERE model = 'Ford';
+
