75af537bff7e7731bda04c9e11d3b6c5684d8912
Timofey Moskvin <alionapermes@gmail.com>
2022-12-14 09:22:44 +0700

    add hash indexes to fkeys

--- semester5/db/v3/src/lab67/index.sql
+++ semester5/db/v3/src/lab67/index.sql
@@ -6,3 +6,8 @@ CREATE INDEX "order_date_idx" ON "order" (date);
 DROP INDEX IF EXISTS "order_season_idx";
 CREATE INDEX "order_season_idx" ON "order" (get_season(date));
 
+DROP INDEX IF EXISTS "order_driver_id_idx";
+CREATE INDEX "order_driver_id_idx" ON "order" USING HASH (driver_id);
+
+DROP INDEX IF EXISTS "destination_city_id_idx";
+CREATE INDEX "destination_city_id_idx" ON "order" USING HASH (destination_city_id);
